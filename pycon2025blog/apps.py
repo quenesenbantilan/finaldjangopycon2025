0118369930c3f0dd4a4a2359ecd01aed05441e7c
Valerie Quenesen Bantilan <vbantilan@abckidsny.com>
2025-03-02 00:31:55 +0800

    First commit

---
+++ pycon2025blog/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Pycon2025BlogConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'pycon2025blog'
---
+++ pycon2025site/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for pycon2025site project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pycon2025site.settings')
+
+application = get_asgi_application()
